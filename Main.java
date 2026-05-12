52802ce0889a85737bb202b777342da33dddb708
alanbrito4007 <alanchurarondo700@gmail.com>
2026-05-11 21:10:00 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@
 public class Main
 {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Ademar eres una prra");
+		System.out.println("pero la queria tanto..");
 	}
 }
